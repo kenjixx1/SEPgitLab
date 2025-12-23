608c6bb129a2a27f03feaefe8aef4e9e511207a4
KenjiX1 <145561863+kenjixx1@users.noreply.github.com>
2025-12-23 18:11:22 +0700

    Kenji Method

--- Question1.py
+++ Question1.py
@@ -23,9 +23,22 @@ class Simple_drawing_window2(Simple_drawing_window):
         p.drawEllipse(50, 50, 150, 150)
         p.end()
 
+
+class Simple_drawing_window3(Simple_drawing_window):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Kenji")
+
+
+    def paintEvent(self, e):
+        p = QPainter(self)
+        p.setBrush(QColor(0, 255, 0))
+        p.drawLine(50, 50, 150, 150)
+        p.end()
+
 def main():
     app = QApplication(sys.argv)
-    w = Simple_drawing_window2()
+    w = Simple_drawing_window3()
     w.show()
     sys.exit(app.exec())
 
