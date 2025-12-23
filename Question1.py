660e889d13f0f98d499bb1e1579cdacb9f508abe
JeenSpcy <jeen1314@gmail.com>
2025-12-23 18:05:59 +0700

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,6 +1,33 @@
 import sys
 from PySide6.QtCore import *
-from PySide6.QtWidgets import *
-from PySide6.QtGui import *
+from PySide6.QtWidgets import*
+from PySide6.QtGui import*
+
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing Github")
+
+    def paintEvent(self, e):
+        pass
+
+class Simple_drawing_window2(Simple_drawing_window):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Jeen")
 
 
+    def paintEvent(self, e):
+        p = QPainter(self)
+        p.setBrush(QColor(0, 255, 0))
+        p.drawEllipse(50, 50, 150, 150)
+        p.end()
+
+def main():
+    app = QApplication(sys.argv)
+    w = Simple_drawing_window2()
+    w.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
