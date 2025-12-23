399ba2b77a4534b72ca241e070c171eaf4e0b382
JeenSpcy <jeen1314@gmail.com>
2025-12-23 19:36:27 +0700

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -2,40 +2,61 @@ import turtle
 
 
 t= turtle.Turtle()
+t.speed(0)
 
-class Disk(object):
-    def __init__(self,name="",xpos=0,ypos=0,height=20,width=40):
+class Disk:
+    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
         self.dname = name
         self.dxpos = xpos
         self.dypos = ypos
         self.dheight = height
         self.dwidth = width
-        
-        
-        
-    
-    def showdisk(self):
-        tempx = self.dxpos - self.dwidth/2
-        tempy = self.dypos - self.dheight/2
-        self.t= turtle.Turtle()
-        self.t.hideturtle()
-        self.t.penup()
-        self.t.goto(tempx,tempy)
-        self.t.pendown()
-        self.t.goto(tempx+self.dwidth,tempy)
-        self.t.goto(tempx+self.dwidth,tempy+self.dheight)
-        self.t.goto(tempx,tempy+self.dheight)
-        self.t.goto(tempx,tempy)
-        self.t.penup()
+    def showDisk(self):
+        t.pencolor("black")
+        t.setheading(0)
+        t.up()
+        t.goto(self.dxpos, self.dypos)
 
-    def cleardisk(self):
-        self.t.reset()
+        t.down()
+        t.forward(self.dwidth//2)
+        t.left(90)
+        t.forward(self.dheight)
+        t.left(90)
+        t.forward(self.dwidth)
+        t.left(90)
+        t.forward(self.dheight)
+        t.left(90)
+        t.forward(self.dwidth//2)
 
-    def newpos(self,x,y):
-        self.dxpos = x
-        self.dypos = y
-        self.cleardisk()
-        self.showdisk()
+        t.setheading(0)
+        t.up()
+        t.goto(self.dxpos, self.dypos)
+    def newpos(self, xpos, ypos):
+        self.clearDisk()
+
+        self.dxpos = xpos
+        self.dypos = ypos
+        
+    def clearDisk(self):
+        t.color("white")
+        t.setheading(0)
+        t.up()
+        t.goto(self.dxpos, self.dypos)
+
+        t.down()
+        t.backward(self.dwidth//2)
+        t.right(90)
+        t.backward(self.dheight)
+        t.right(90)
+        t.backward(self.dwidth)
+        t.right(90)
+        t.backward(self.dheight)
+        t.right(90)
+        t.backward(self.dwidth//2)
+
+        t.setheading(0)
+        t.up()
+        t.goto(self.dxpos, self.dypos)
 
 class Pole:
     def __init__(self, name="", xpos=0, ypos=0, thick=10, length=100):
@@ -84,48 +105,33 @@ class Pole:
 
 
 
-
-t.screen.mainloop()
-
-
-# import turtle
-
-# t1 = turtle.Turtle()
-# t2 = turtle.Turtle()
-# t1.circle(50)
-# t2.forward(100)
-
-# # Clear the entire screen and remove all turtles
-# t1.reset()
-
-# turtle.done()
-
 class Hanoi(object):
-    def init(self, n=3, start="A", workspace="B", destination="C"):
-        self.startp = Pole(start, 0, 0)
-        self.workspacep = Pole(workspace, 150, 0)
-        self.destinationp = Pole(destination, 300, 0)
-        self.startp.showpole()
-        self.workspacep.showpole()
-        self.destinationp.showpole()
+    def __init__(self,n=3,start="A",workspace="B",destination="C"):
+        self.startp = Pole(start,0,0)
+        self.workspacep = Pole(workspace,150,0)
+        self.destinationp = Pole(destination,300,0)
+        self.startp.showPole()
+        self.workspacep.showPole()
+        self.destinationp.showPole()
         for i in range(n):
-            self.startp.pushdisk(Disk("d"+str(i), 0, i*150, 20, (n-i)30))
+            self.startp.pushDisk(Disk("d"+str(i),0,i*150,20,(n-i)*30))
 
-    def move_disk(self, start, destination):
-        disk = start.popdisk()
-        destination.pushdisk(disk)
-
-    def move_tower(self, n, s, d, w):
+    def move_disk(self,start,destination):
+        disk = start.popDisk()
+        destination.pushDisk(disk)
+    def move_tower(self,n,s,d,w):
         if n == 1:
-            self.move_disk(s, d)
+            self.move_disk(s,d)
         else:
-            self.move_tower(n-1, s, w, d)
-            self.move_disk(s, d)
-            self.move_tower(n-1, w, d, s)
-
+            self.move_tower(n-1,s,w,d)
+            self.move_disk(s,d)
+            self.move_tower(n-1,w,d,s)
     def solve(self):
-        self.move_tower(3, self.startp, self.destinationp, self.workspacep)
-
+        self.move_tower(3,self.startp,self.destinationp,self.workspacep)
 
 h = Hanoi()
 h.solve()
+
+
+
+
