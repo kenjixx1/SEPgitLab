a372bb108b4ebf56a5b52944f582f15352b9e65e
giraffeDek <pompompurinstory@gmail.com>
2025-12-23 19:18:23 +0700

    pole

--- Question3.py
+++ Question3.py
@@ -10,18 +10,22 @@ class Disk(object):
         self.dypos = ypos
         self.dheight = height
         self.dwidth = width
-        self.t= turtle.Turtle()
+        
+        
         
     
     def showdisk(self):
+        tempx = self.dxpos - self.dwidth/2
+        tempy = self.dypos - self.dheight/2
         self.t= turtle.Turtle()
+        self.t.hideturtle()
         self.t.penup()
-        self.t.goto(self.dxpos,self.dypos)
+        self.t.goto(tempx,tempy)
         self.t.pendown()
-        self.t.goto(self.dxpos+self.dwidth,self.dypos)
-        self.t.goto(self.dxpos+self.dwidth,self.dypos+self.dheight)
-        self.t.goto(self.dxpos,self.dypos+self.dheight)
-        self.t.goto(self.dxpos,self.dypos)
+        self.t.goto(tempx+self.dwidth,tempy)
+        self.t.goto(tempx+self.dwidth,tempy+self.dheight)
+        self.t.goto(tempx,tempy+self.dheight)
+        self.t.goto(tempx,tempy)
         self.t.penup()
 
     def cleardisk(self):
@@ -33,17 +37,53 @@ class Disk(object):
         self.cleardisk()
         self.showdisk()
 
+class Pole:
+    def __init__(self, name="", xpos=0, ypos=0, thick=10, length=100):
+        self.name = name
+        self.xpos = xpos
+        self.ypos = ypos
+        self.thick = thick
+        self.length = length
+
+        self.stack = []
+        self.toppos = 0
+    def showPole(self):
+        t.pu()
+        t.goto(self.xpos, self.ypos)
+        t.pd()
+        t.forward(self.thick / 2)
+        t.left(90)
+        t.forward(self.length)
+        t.left(90)
+        t.forward(self.thick)
+        t.left(90)
+        t.forward(self.length)
+        t.left(90)
+        t.forward(self.thick / 2)
+    def pushDisk(self, disk):
+        if disk:
+            disk.newpos(self.xpos, self.ypos + self.toppos*20)
+            self.stack.append(disk)
+            self.toppos += 1
+            disk.showDisk()
+        
+    def popDisk(self):
+        print("popping from", self.name)
+        if len(self.stack) != 0:
+            disk = self.stack.pop()
+            self.toppos -= 1
+            disk.clearDisk()
+            disk.newpos(disk.dxpos, 120)
+            disk.showDisk()
+            return disk
+
 
         
 
-h=Disk(xpos=30,ypos=50)
-h.showdisk()
-
-a=Disk(xpos=100,ypos=50)
-a.showdisk()
 
 
-h.newpos(200,300)
+
+
 
 t.screen.mainloop()
 
