e5da6f50fef53e134f14d93f8ebbec8ca30c5621
KenjiX1 <145561863+kenjixx1@users.noreply.github.com>
2025-12-23 18:13:49 +0700

    Layout

--- Question2.py
+++ Question2.py
@@ -5,3 +5,18 @@ class Transportation(ABC):
         self.start = s
         self.end = e
         self.distance = d
+
+    @abstractmethod
+    def calculate(self):
+        pass
+
+
+class Walk(Transportation):
+    def __init__(self, s, e, d):
+        super().__init__(s, e, d)
+    
+    def calculate(self):
+        return 0
+    
+
+
